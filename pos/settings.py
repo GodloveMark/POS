33f707144be236c1b0f4ae6c3601d6bab2da3738
GodloveMark <39481193+GodloveMark@users.noreply.github.com>
2026-02-17 02:04:52 +0300

    new Updates

--- pos/settings.py
+++ pos/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
+# Load environment variables
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,14 +24,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--w(dug*va_megpa25w$gmv8o6+f8!fg_*m1^u$)z25pab0hicm'
+SECRET_KEY = os.getenv('SECRET_KEY', 'fallback-secret-key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 # Only do this in development or when debugging deploys
 WHITENOISE_MANIFEST_STRICT = False
-
+ 
 ALLOWED_HOSTS = [
     'localhost',
     '127.0.0.1',
